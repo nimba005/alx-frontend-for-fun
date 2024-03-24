4fa94cfd80490498da8fcfbc2afe5e449277408a
nimba005 <nimbaoyugi@gmail.com>
2024-03-24 11:59:39 +0300

    markdown

---
+++ README.md
@@ -0,0 +1,15 @@
+# My title
+
+- He**l**lo
+- Bye
+
+Hello
+
+I'm **a** text
+with **2 lines**
+
+((I will live in Caracas))
+
+But it's [[private]]
+
+So cool!
---
+++ markdown2html.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+"""markdown2html"""
+import sys
+import os
+import re
+import hashlib
+
+
+def markdown2html(markdown, output):
+    """
+    markdown2html: Markdown to HTML
+
+    Args:
+        markdown: Markdown file
+        output: output file name
+    """
+
+    if not os.path.isfile(markdown):
+        print(f"Missing {markdown}", file=sys.stderr)
+        sys.exit(1)
+
+    with open(markdown, 'r') as f:
+        lines = f.readlines()
+
+    with open(output, 'w') as f:
+        list_item = False
+        ordered_list = False
+        contd_paragraph = False
+
+        for line in lines:
+            line = line.strip()
+
+            line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)
+            line = re.sub(r'__(.*?)__', r'<em>\1</em>', line)
+
+            if '[[' in line and ']]' in line:
+                text = line[line.index('[[') + 2:line.index(']]')]
+                encoded_text = hashlib.md5(text.encode())
+                line = line.replace(f'[[{text}]]', f'{encoded_text.hexdigest()}')
+
+            if '((' in line and '))' in line:
+                text = line[line.index('((') + 2:line.index('))')]
+                new_text = text.replace('c', '')
+                new_text = new_text.replace('C', '')
+                line = line.replace(f'(({text}))', f'{new_text}')
+
+            if line.startswith('#'):
+                heading_count = line.count('#')
+                heading_text = line.strip('#').strip()
+
+                html = f'<h{heading_count}>{heading_text}</h{heading_count}>'
+                f.write(f'{html}\n')
+
+            elif line.startswith('-'):
+                if not list_item:
+                    f.write('<ul>\n')
+                    list_item = True
+                f.write(f"  <li>{line.strip('-').strip()}</li>\n")
+
+            elif line.startswith('*'):
+                if not ordered_list:
+                    f.write('<ol>\n')
+                    ordered_list = True
+                f.write(f"  <li>{line.strip('*').strip()}</li>\n")
+
+            else:
+                if line:
+                    if not contd_paragraph:
+                        f.write('<p>\n')
+                        contd_paragraph = True
+                    else:
+                        f.write('<br/>\n')
+                    f.write(f'{line}\n')
+                else:
+                    if contd_paragraph:
+                        f.write('</p>\n')
+                        contd_paragraph = False
+
+                if list_item:
+                    f.write('</ul>\n')
+                    list_item = False
+                elif ordered_list:
+                    f.write('</ol>\n')
+                    ordered_list = False
+
+        if contd_paragraph:
+            f.write('</p>\n')
+
+        if list_item:
+            f.write('</ul>\n')
+        elif ordered_list:
+            f.write('</ol>\n')
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print(
+            'Usage: ./markdown2html.py README.md README.html',
+            file=sys.stderr
+            )
+        sys.exit(1)
+
+    if isinstance(sys.argv[1], str) and isinstance(sys.argv[2], str):
+        markdown2html(sys.argv[1], sys.argv[2])
